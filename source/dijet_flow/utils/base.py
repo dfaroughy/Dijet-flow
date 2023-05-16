540a882cb88c73bb19e95a04e12949e1bc145f97
Darius Faroughy <df630@pascal2.rutgers.edu>
2023-05-16 18:06:42 -0400

    added ep2ptepm function

--- source/dijet_flow/utils/base.py
+++ source/dijet_flow/utils/base.py
@@ -62,3 +62,25 @@ def save_arguments(args, name):
     args_dict = {a: serialize(b) for a, b in vars(args).items()}
     with open(args.workdir+'/'+name, 'w') as file: json.dump(args_dict, file, indent=4)
 
+
+def ep2ptepm(vec):
+    """ Convert (px, py, pz, e) into (pT, eta, phi, mass)
+        for torch.tensors.
+    """
+    vec_ = torch.zeros_like(vec)
+    p = torch.sqrt(vec[:,0]**2 + vec[:,1]**2 + vec[:,2]**2)  # |p|
+    cos = vec[:,3] / p  # cos(theta)
+    cos[p==0] = 1.0
+    cos_0 = cos**2 < 1
+    m2 = vec[:,3]**2 - p**2
+    m2[m2 < 0] *= -1.0
+    vec_[:,0] = torch.sqrt(vec[:,0]**2 + vec[:,1]**2)   # pT
+    vec_[:,1][cos_0] = -0.5 * torch.log(1. - cos) + 0.5 * torch.log(1. + cos)   # eta
+    vec_[:,1][~cos_0 & (vec[:,2] == 0.)] = 0.
+    vec_[:,1][~cos_0 & (vec[:,2] > 0.)] = 10e10
+    vec_[:,1][~cos_0 & (vec[:,2] < 0.)] = -10e10
+    vec_[:,2] = torch.arctan2(vec[:,1], vec[:,0])     # phi
+    vec_[:,2][(vec[:,1] == 0) & (vec[:,0] == 0)] = 0.
+    vec_[:,3] = np.sqrt(m2)      # mass
+    vec_[:,3][m2 < 0] *= -1.0
+    return vec_
