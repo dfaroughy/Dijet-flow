81acade9158cc6a989251a546d1687f4536be875
Darius Faroughy <df630@pascal2.rutgers.edu>
2023-05-18 10:24:11 -0400

    added plot routines

--- source/dijet_flow/data/plots.py
+++ source/dijet_flow/data/plots.py
@@ -5,69 +5,9 @@ from scipy.stats import gaussian_kde
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy import stats
-from dijet_flow.utils.base import savefig
+from dijet_flow.utils.base import savefig, deltaR, dphi, deta
 
 sns.set_theme(style="dark")
-
-def plot_data_projections(sample, 
-                          title, 
-                          save_dir, 
-                          num_stars=20000, 
-                          bin_size=None,
-                          xlim=[(-10, 10), (-10, 10), (-10, 10)], 
-                          ylim=[(-10, 10), (-10, 10), (-10, 10)], 
-                          figsize=(15, 10),
-                          label='x',
-                          cmap="magma"):
-
-    print("INFO: plotting -> {}".format(title))
-
-    if label == 'x': label = [r"$x$ (kpc)", r"$y$ (kpc)", r"$z$ (kpc)"]
-    elif label == 'v': label = [r"$v_x$ (km/s)", r"$v_y$ (km/s)", r"$v_z$ (km/s)"]
-    else: pass
-    
-    # Extract x, y, and z coordinates
-    x = sample[:num_stars, 0]#.numpy()
-    y = sample[:num_stars, 1]#.numpy()
-    z = sample[:num_stars, 2]#.numpy()
-
-    # Create a list of tuples for each projection
-    projections = [((x, y), label[0], label[1]), ((y, z), label[1], label[2]), ((z, x),label[2], label[0])]
-
-    fig, axes = plt.subplots(2, 3, figsize=figsize)
-
-    for idx, ((data_x, data_y), xlabel, ylabel) in enumerate(projections):
-
-        if bin_size:
-            bin_edges_x = np.linspace(xlim[idx][0], xlim[idx][1], int((xlim[idx][1] - xlim[idx][0]) / bin_size) + 1)
-            bin_edges_y = np.linspace(ylim[idx][0], ylim[idx][1], int((ylim[idx][1] - ylim[idx][0]) / bin_size) + 1)
-            bins = (bin_edges_x, bin_edges_y)
-        else: 
-            bins = bin_edges_x = 100
-
-        sns.histplot(x=data_x, bins=bin_edges_x, color='k', ax=axes[0, idx], element="step", kde = True, alpha=0.4)
-
-        axes[0, idx].set_xlim(xlim[idx])
-        axes[0, idx].set_xlabel(xlabel)
-        axes[0, idx].set_ylabel('counts')
-        axes[0, idx].grid()
-
-        sns.kdeplot(x=data_x, y=data_y, levels=6, color="w", linewidths=1, ax=axes[1, idx])
-        sns.scatterplot(x=data_x, y=data_y, s=5, color=".15", ax=axes[1, idx])
-        sns.histplot(x=data_x, y=data_y, bins=bins, cmap=cmap, ax=axes[1, idx])
-
-        axes[1, idx].set_xlim(xlim[idx])
-        axes[1, idx].set_ylim(ylim[idx])
-        axes[1, idx].set_xlabel(xlabel)
-        axes[1, idx].set_ylabel(ylabel)
-        axes[1, idx].grid()
-
-    fig.suptitle(title)
-    fig.tight_layout()
-    plt.savefig(save_dir+'/{}.png'.format(title.replace(" ", "_")))
-
-
-
 
 def plot_loss(train, valid, args):
     train_loss = train.loss_per_epoch
@@ -81,4 +21,56 @@ def plot_loss(train, valid, args):
     plt.title("loss_min={}, epochs={}".format(round(loss_min,6),len(train_loss)))
     fig.tight_layout()
     plt.grid() 
-    plt.savefig(savefile(args.workdir+'/loss.png', extension="png"))
+    plt.savefig(savefig(args.workdir+'/loss.png', extension="png"))
+
+
+def jet_plot_routine( data, 
+                      title, 
+                      save_dir,  
+                      bins=100,
+                      xlim=[(500, 2500), (-2.5, 2.5), (-4, 4), (0, 800)], 
+                      figsize=(20, 15)
+                    ):
+
+    print("INFO: plotting -> {}".format(title))
+    jet1 = [ data[:, 0], data[:, 1], data[:, 2], data[:, 3] ]
+    jet2 = [ data[:, 4], data[:, 5], data[:, 6], data[:, 7] ]
+    low_level_feat = [r'$p_t$ (GeV)',r'$\eta$',r'$\phi$',r'$m$ (GeV)']
+    xlim_llf = [(500, 2500), (-2.5, 2.5), (-4, 4), (0, 800)]
+
+    mjj = data[:,-1]
+    del_phi = dphi(data[:, :4], data[:, 4:8])
+    del_eta = deta(data[:, :4], data[:, 4:8])
+    del_R = deltaR(data[:, :4], data[:, 4:8])
+    dijet = [mjj, del_eta, del_phi, del_R ]
+    high_level_feat = [r'$m_{jj}$ (GeV)', r'$\Delta\eta$',r'$\Delta\phi$',r'$\Delta R$']
+    xlim_hlf = [(1000, 6000), (-5, 5), (-5, 5), (0, 5)]
+
+
+    fig, axes = plt.subplots(3, 4, figsize=figsize)
+
+    for idx, llf  in enumerate(low_level_feat):
+
+        sns.histplot(x=jet1[idx], bins=bins, color='k', ax=axes[0, idx], element="step", kde = True, alpha=0.4)
+        axes[0, idx].set_xlim(xlim_llf[idx])
+        axes[0, idx].set_xlabel(llf+' jet 1')
+        axes[0, idx].set_ylabel('counts')
+        axes[0, idx].grid()
+
+        sns.histplot(x=jet2[idx], bins=bins, color='k', ax=axes[1, idx], element="step", kde = True, alpha=0.4)
+        axes[1, idx].set_xlim(xlim_llf[idx])
+        axes[1, idx].set_xlabel(llf+' jet 2')
+        axes[1, idx].set_ylabel('counts')
+        axes[1, idx].grid()
+    
+    for idx, hlf  in enumerate(high_level_feat):
+        sns.histplot(x=dijet[idx], bins=bins, color='k', ax=axes[2, idx], element="step", kde = True, alpha=0.4)
+        axes[2, idx].set_xlim(xlim_hlf[idx])
+        axes[2, idx].set_xlabel(hlf)
+        axes[2, idx].set_ylabel('counts')
+        axes[2, idx].grid()
+
+    fig.suptitle(title)
+    fig.tight_layout()
+    plt.savefig(save_dir+'/{}.png'.format(title.replace(" ", "_")))
+
--- source/dijet_flow/data/transform.py
+++ source/dijet_flow/data/transform.py
@@ -3,48 +3,104 @@ import numpy as np
 import pandas as pd
 import torch
 
-from dijet_flow.data.plots import plot_data_projections
+from dijet_flow.data.plots import jet_plot_routine
+from dijet_flow.utils.base import em2ptepm, logit, expit, inv_mass
 
 
 class Transform:
 
-    def __init__(self, data, args):
+    def __init__(self, data, args, convert_to_ptepm=True):
 
         self.args = args
-        self.data = data    
-        self.mean = torch.zeros(args.dim)
-        self.std = torch.zeros(args.dim)
+        self.data = data  
+        if convert_to_ptepm:
+            self.data[:, :4] = em2ptepm(data[:, :4])    # input is in 'em' coords: (px,py,pz,m)
+            self.data[:, 4:8] = em2ptepm(data[:, 4:8])   
+        self.min = None
+        self.max = None 
+        self.mean = torch.zeros(8)
+        self.std = torch.zeros(8)
 
     @property
     def leading(self):
         return self.data[:, :4]
     @property
     def subleading(self):
-        return self.data[:, 7:11]
+        return self.data[:, 4:8]
     @property
-    def jj(self):
-        return np.concatenate((self.data[:, :4], self.data[:, 7:11]), axis=1)
+    def dijet(self):
+        return self.data[:, :8]
     @property
-    def mass(self):
-        return self.data[:, -2]
-    @property
-    def truth_label(self):
+    def mjj(self):
         return self.data[:, -1]
     @property
-    def num_events(self):
+    def num_jets(self):
         return self.data.shape[0]
+
+    def compute_mjj(self):
+        self.data[:,-1] = inv_mass(self.data[:, :4], self.data[:, 4:8], coord='ptepm')
+        return self
 
     def get_sidebands(self):
         m0, m1, m2, m3 = self.args.mass_window
-        mjj = self.data[:,-2]
+        mjj = self.data[:,-1]
         self.data = self.data[ ((m0 < mjj) & (mjj < m1)) | ((m2 < mjj) & (mjj < m3))]
         return self
 
-
-    def logit(self, alpha=1e-6, inverse=False):
-        
-        if not inverse:
-            self.jj = alpha + (1 - 2 * alpha) * self.jj
-            self.jj = torch.log(self.jj/(1-self.jj))
-
+    def get_signal_region(self):
+        _, m1, m2, _ = self.args.mass_window
+        mjj = self.data[:,-1]
+        self.data = self.data[(m1 <= mjj) & (mjj <= m2)]
         return self
+
+    def normalize(self, inverse=False):
+        if not inverse:
+            self.max = torch.max(self.dijet)
+            self.min = torch.min(self.dijet)
+            self.data[:, :4] = (self.data[:, :4] - self.min) / (self.max - self.min) 
+            self.data[:, 4:8] = (self.data[:, 4:8] - self.min) / (self.max - self.min)
+        else:
+            self.data[:, :4] = self.data[:, :4] * (self.max - self.min) + self.min
+            self.data[:, 4:8] = self.data[:, 4:8] * (self.max - self.min) + self.min 
+        return self
+
+    def standardize(self, inverse=False):
+        if not inverse:
+            self.mean[:4] = torch.mean(self.data[:, :4], dim=0)
+            self.std[:4] = torch.std(self.data[:, :4], dim=0)
+            self.mean[4:] = torch.mean(self.data[:, 4:8], dim=0)
+            self.std[4:] = torch.std(self.data[:, 4:8], dim=0)
+            self.data[:, :4] = (self.leading - self.mean[:4]) / self.std[:4]
+            self.data[:, 4:8] = (self.subleading - self.mean[4:]) / self.std[4:]
+        else:
+            self.data[:, :4] = self.leading * self.std[:4] + self.mean[:4]
+            self.data[:, 4:8] = self.subleading * self.std[4:] + self.mean[4:]
+        return self
+
+    def logit_transform(self, alpha=1e-6, inverse=False):
+        if not inverse:
+            self.data[:, :4] = logit(self.data[:, :4], alpha=alpha)
+            self.data[:, 4:8] = logit(self.data[:, 4:8], alpha=alpha)
+        else:
+            self.data[:, :4] = expit(self.data[:, :4], alpha=alpha)
+            self.data[:, 4:8] = expit(self.data[:, 4:8], alpha=alpha)
+        return self
+
+    def preprocess(self, alpha=1e-6, reverse=False, verbose=True):
+        if not reverse:
+            if verbose: print('INFO: reversing preprocessed data')
+            self.normalize()
+            self.logit_transform(alpha=alpha)
+            self.standardize()
+        else:
+            if verbose: print('INFO: preprocessing data')
+            self.standardize(inverse=True)
+            self.logit_transform(alpha=alpha, inverse=True)
+            self.normalize(inverse=True)
+        return self
+
+    def plot_jet_features(self, title, bins=100, save_dir=None):
+        if not save_dir: save_dir = self.args.workdir
+        jet_plot_routine(self.data, bins=bins, title=title, save_dir=save_dir)
+
+
