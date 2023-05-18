385d98bd79ade0c2122b3f4962c1f084aeebe288
Darius Faroughy <df630@pascal2.rutgers.edu>
2023-05-18 10:31:10 -0400

    update params

--- main/flow.py
+++ main/flow.py
@@ -15,10 +15,10 @@ from dijet_flow.utils.base import make_dir, copy_parser, save_arguments
 from dijet_flow.models.flows.norm_flows import masked_autoregressive_flow, coupling_flow
 from dijet_flow.models.training import Model
 from dijet_flow.models.loss import neglogprob_loss
-from dijet_flow.data.transform import Transform
+from dijet_flow.data.transform import Transform 
 
 sys.path.append("../")
-# torch.set_default_dtype(torch.float64)
+torch.set_default_dtype(torch.float64)
 
 
 '''
@@ -29,18 +29,15 @@ sys.path.append("../")
 
     The flow generates context for a diffusion model for anomaly detection.
 
-    tips:
-
-
-
+ 
 '''
 
 ####################################################################################################################
 
 params = argparse.ArgumentParser(description='arguments for the flow model')
 
-params.add_argument('--workdir')
-params.add_argument('--device',       default='cuda:1',           help='where to train')
+params.add_argument('--workdir',      help='working directory',   type=str)
+params.add_argument('--device',       default='cuda:0',           help='where to train')
 params.add_argument('--dim',          default=8,                  help='dimensionalaty of data: (x,y,z,vx,vy,vz)', type=int)
 params.add_argument('--loss',         default=neglogprob_loss,    help='loss function')
 
@@ -51,36 +48,33 @@ params.add_argument('--dim_flow',     default=8,            help='dimension of i
 params.add_argument('--flow_func',    default='RQSpline',   help='type of flow transformation: affine or RQSpline', type=str)
 params.add_argument('--coupl_mask',   default='mid-split',  help='mask type [only for coupling flows]: mid-split or checkers', type=str)
 params.add_argument('--permutation',  default='inverse',    help='type of fixed permutation between flows: n-cycle or inverse', type=str)
-params.add_argument('--num_flows',    default=10,            help='num of flow layers', type=int)
+params.add_argument('--num_flows',    default=32,            help='num of flow layers', type=int)
 params.add_argument('--dim_hidden',   default=128,          help='dimension of hidden layers', type=int)
 params.add_argument('--num_spline',   default=30,           help='num of spline for rational_quadratic', type=int)
 params.add_argument('--num_blocks',   default=2,            help='num of MADE blocks in flow', type=int)
-params.add_argument('--dim_context',  default=None,         help='dimension of context features', type=int)
+params.add_argument('--dim_context',  default=1,         help='dimension of context features', type=int)
 
 #...training params:
 
 params.add_argument('--batch_size',    default=1024,          help='size of training/testing batch', type=int)
 params.add_argument('--num_steps',     default=0,            help='split batch into n_steps sub-batches + gradient accumulation', type=int)
 params.add_argument('--test_size',     default=0.2,          help='fraction of testing data', type=float)
-params.add_argument('--max_epochs',    default=1000,         help='max num of training epochs', type=int)
+params.add_argument('--max_epochs',    default=20,            help='max num of training epochs', type=int)
 params.add_argument('--max_patience',  default=20,           help='terminate if test loss is not changing', type=int)
 params.add_argument('--lr',            default=1e-4,         help='learning rate of generator optimizer', type=float)
 params.add_argument('--activation',    default=F.leaky_relu, help='activation function for neural networks')
 params.add_argument('--batch_norm',    default=True,         help='apply batch normalization layer to flow blocks', type=bool)
 params.add_argument('--dropout',       default=0.1,          help='dropout probability', type=float)
+params.add_argument('--seed',          default=999,          help='random seed for data split', type=int)
 
 #... data params:
 
-params.add_argument('--mass_window',  default=(3000,3300,
-                                               3400,3700),  help='mass window: SB1, SR, SB2', type=tuple)
-
-# params.add_argument('--',       default=               help='', type=)
-# params.add_argument('--',       default=               help='', type=)
-# params.add_argument('--',       default=               help='', type=)
-# params.add_argument('--',       default=               help='', type=)
-# params.add_argument('--',       default=               help='', type=)
-# params.add_argument('--',       default=               help='', type=)
-
+params.add_argument('--mass_window', default=(3000,3300,3400,3700), help='bump hunt mass window: SB1, SR, SB2', type=tuple)
+params.add_argument('--num_gen',     default=10000,                  help='number of sampled events from model', type=int)
+params.add_argument('--mean',        default=[],                    help='data mean (for preprocessing)', type=list)
+params.add_argument('--std',         default=[],                    help='data covariance (for preprocessing)', type=list)
+params.add_argument('--min',         default=None,                  help='data min (for preprocessing)', type=float)
+params.add_argument('--max',         default=None,                  help='data max (for preprocessing)', type=float)
 
 
 ####################################################################################################################
@@ -90,80 +84,72 @@ if __name__ == '__main__':
     #...create working folders and save args
 
     args = params.parse_args()
-    args.workdir = make_dir('Results_Dijets_density', sub_dirs=['data_plots', 'results_plots'], overwrite=True)
+    args.workdir = make_dir('Results_dijet_density', sub_dirs=['data_plots', 'results_plots'], overwrite=False)
     print("#================================================")
     print("INFO: working directory: {}".format(args.workdir))
     print("#================================================")
 
     #...get datasets, preprocess them
 
-    data_file =  "./data/events_anomalydetection_v2.features_with_jet_constituents.h5"
-    lhco = pd.read_hdf(data_file)
-    lhco = torch.tensor(lhco.to_numpy())
+    file =  "./data/events_anomalydetection_v2.features_with_jet_constituents.h5"
+    data = torch.tensor(pd.read_hdf(file).to_numpy())
+    data = torch.cat((data[:, :4], data[:, 7:11], torch.unsqueeze(data[:, -2], dim=1)), dim=1)  # d=9: (jet1, jet2, mjj)
 
-    dijets = Transform(lhco, args)
+    #...get SB events and preprocess data
 
-    dijets.get_sidebands() # dijets from both Side bands
-    dijets.logit() # dijets from both Side bands
+    events_SB = Transform(data, args)
+    events_SR = Transform(data, args)
 
-    dijets.get_sidebands() # dijets from both Side bands
+    # events.plot_jet_features(title='jet features data', save_dir=args.workdir+'/data_plots')
+    events_SB.get_sidebands()
+    events_SB.plot_jet_features(title='jet features sidebands', save_dir=args.workdir+'/data_plots')
+    events_SB.preprocess()
 
-    print(dijets.leading[0], dijets.subleading[0], dijets.jj[0])
-    #...smear and preprocess data    
+    events_SR.get_signal_region() 
+    events_SR.plot_jet_features(title='jet features signal region', save_dir=args.workdir+'/data_plots')
+    events_SR.preprocess()
 
-    # dijet = Transform(data)
+    #...store parser arguments
 
+    args.num_jets = events_SB.num_jets
+    args.num_gen = events_SB.num_jets
+    args.mean = events_SB.mean.tolist()
+    args.std = events_SB.std.tolist()
+    args.max = events_SB.max.tolist()
+    args.min = events_SB.min.tolist()
+    print("INFO: num jets: {}".format(args.num_jets))
+    save_arguments(args, name='inputs.json')
 
+    # #...Prepare train/test samples from sidebands
 
-
-
-
-    # gaia.get_stars_near_sun(self, R=args.radius)
-
-    # if args.data == 'noisy': gaia.smear()
-
-    # gaia.plot('x', title='target positions', save_dir=args.workdir+'/data_plots') 
-    # gaia.plot('v', title='target velocities', save_dir=args.workdir+'/data_plots') 
-    
-    # gaia.preprocess()
-
-    # #...store parser arguments
-
-    # args.num_stars = gaia.num_stars
-    # args.mean = gaia.mean.tolist()
-    # args.std = gaia.std.tolist()
-    # args.Rmax = gaia.Rmax.tolist()
-    # print("INFO: num stars: {}".format(args.num_stars))
-    # save_arguments(args, name='inputs.json')
-
-    # #...Prepare train/test samples
-
-    # train, test  = train_test_split(gaia.data, test_size=args.test_size, random_state=9999)
-    # train_sample = DataLoader(dataset=torch.Tensor(train), batch_size=args.batch_size, shuffle=True)
-    # test_sample  = DataLoader(dataset=torch.Tensor(test),  batch_size=args.batch_size, shuffle=False)
+    train, test  = train_test_split(events_SB.data, test_size=args.test_size, random_state=args.seed)
 
     # #...define model
 
-    # if args.flow == 'MAF': flow = masked_autoregressive_flow(args)
-    # elif args.flow == 'coupling': flow = coupling_flow(args)
+    if args.flow == 'MAF': flow = masked_autoregressive_flow(args)
+    elif args.flow == 'coupling': flow = coupling_flow(args)
+    flow = flow.to(args.device)
+    model = Model(flow) 
 
-    # flow = flow.to(args.device)
+    #...train flow for density estimation.
 
-    # #...train flow for phase-space density estimation.
+    print("INFO: start training")
+    train_sample = DataLoader(dataset=torch.Tensor(train), batch_size=args.batch_size, shuffle=True)
+    test_sample  = DataLoader(dataset=torch.Tensor(test),  batch_size=args.batch_size, shuffle=False)
+    model.train(train_sample, test_sample, args)
 
-    # Train_Model(flow, train_sample, test_sample, args , show_plots=False, save_best_state=False)
+    # sample from model:
     
-    # #...sample from flow model
+    sample = model.sample(num_samples=args.num_gen)
+    sample = torch.cat((sample, torch.zeros(sample.shape[0],1)), dim=1)
+    events_gen = Transform(sample, args, convert_to_ptepm=False)
+    events_gen.mean = events.mean
+    events_gen.std = events.std
+    events_gen.min = events.min
+    events_gen.max = events.max
+    events_gen.preprocess(reverse=True)
+    events_gen.compute_mjj()
+    events_gen.plot_jet_features(title='generated features', save_dir=args.workdir+'/results_plots')
 
-    # sample = sampler(flow, num_samples=args.num_gen)
 
-    # #...transofrm back to phase-space amd plot
 
-    # gaia_sample = GaiaTransform(sample, torch.zeros(sample.shape), args) 
-    # gaia_sample.mean = gaia.mean
-    # gaia_sample.std =  gaia.std
-    # gaia_sample.preprocess(R=gaia.Rmax, reverse=True) # invert preprocess transformations
-    # gaia_sample.plot('x', title='position density', save_dir=args.workdir+'/results_plots') 
-    # gaia_sample.plot('v', title='velocity density', save_dir=args.workdir+'/results_plots') 
-
-    # 
